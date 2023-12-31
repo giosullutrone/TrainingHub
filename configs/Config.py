de82432b140e957c2161437d9c66aa34d07fdf19
Giovanni Sullutrone <giovisull@gmail.com>
2023-12-31 21:09:39 +0100

    Fixed Config class ordering of variables

--- configs/Config.py
+++ configs/Config.py
@@ -31,21 +31,18 @@ class Config:
         - `completion_only` (bool): Whether to train on prompt completion only or to predict the whole prompt. Default: True.
         - `system_tuning` (bool): Whether to train `prompt tuning` on system prompts instead of the naive prepending. Only applicable if using `prompt tuning` as peft Default: False.
     """
-    # Dataset
-    dataset_name: Union[str, None] = field(default=None)
     dataset_recipe: DatasetRecipe
-    validation_split_size: Union[float, None] = field(default=None)
-    num_examples: int = field(default=0)
-
-    # Model
-    model_name: Union[str, None] = field(default=None)
     model_recipe: ModelRecipe
     model_template_recipe: ModelTemplateRecipe
     tokenizer_recipe: TokenizerRecipe
     quantization_recipe: Union[QuantizationRecipe, None] = field(default=None)
     peft_recipe: Union[PeftRecipe, None] = field(default=None)
+    
+    dataset_name: Union[str, None] = field(default=None)
+    model_name: Union[str, None] = field(default=None)
+    validation_split_size: Union[float, None] = field(default=None)
+    num_examples: int = field(default=0)
 
-    # Training
     training_arguments: Union[TrainingArguments, None] = field(default=None)
     finetuner_arguments: Union[Dict, None] = field(default_factory=dict)
     completion_only: bool = field(default=True)

36b83cad11acbe864b5cc400dba58cc6a1a155b2
Giovanni Sullutrone <giovisull@gmail.com>
2023-12-31 21:25:42 +0100

    Fixed train_cli fit system template

--- train_cli.py
+++ train_cli.py
@@ -190,7 +190,7 @@ if __name__ == "__main__":
     # 
     # This should make the tuning better mimic the performances obtained for the typical prompt engineering done by hand
     if peft_recipe is not None and peft_recipe.peft_config_obj == PromptTuningConfig and config.system_tuning:
-        system_template = fit_system_template_tokens(dataset_train, model_recipe, tokenizer)
+        system_template = fit_system_template_tokens(dataset_train, model_template_recipe, tokenizer)
         model = SystemTuning.add_proxy(model, system_template=system_template)
     # --------------------------------------------------------------------------
 

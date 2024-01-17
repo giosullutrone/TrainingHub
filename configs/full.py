4de28c1f4cd18b086ff888065ce7d48fa14d3c5e
Giovanni Sullutrone <giovisull@gmail.com>
2024-01-17 11:34:15 +0100

    Added logging at first step for full.py

--- configs/full.py
+++ configs/full.py
@@ -50,6 +50,7 @@ config = ConfigTrain(
         "save_total_limit": 1,
         "per_device_train_batch_size": 1,
         "per_device_eval_batch_size": 2,
+        "logging_first_step": True
     },
     finetuner_arguments={
         "max_seq_length": 512

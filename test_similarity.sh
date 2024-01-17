bf4eaa3bcc28e0653f6836df72c2e75b6c6a6469
Giovanni Sullutrone <giovisull@gmail.com>
2024-01-17 02:07:37 +0100

    Fixed peft training: default tokenizer was being used

--- test_similarity.sh
+++ test_similarity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/app/eval_llm/bin/python3 similarity_cli.py -c configs/similarity_yaml.py \
+python similarity_cli.py -c configs/similarity_yaml.py \
 --output_path "../models/mistral/mathqa/result.json" \
 --dataset_name "math_qa" \
 --dataset_recipe "YAMLDatasetRecipe" \
@@ -8,7 +8,45 @@
 --num_examples 3 \
 --model_name "mistralai/Mistral-7B-Instruct-v0.1" \
 --tokenizer_name "mistralai/Mistral-7B-Instruct-v0.1" \
---model_recipe "MistralModelRecipe" --model_load '{"cache_dir": "../models"}' \
---tokenizer_recipe "MistralTokenizerRecipe" --tokenizer_load '{"cache_dir": "../models"}' \
---finetune_path "../models/mistral/mathqa/finetune_3shot/checkpoint-3357" \
---peft_path "../models/mistral/mathqa/spt_3shot/checkpoint-3357"
+--model_recipe "MistralModelRecipe" \
+--model_template_recipe "MistralModelTemplateRecipe" \
+--model_load '{"cache_dir": "../models"}' \
+--tokenizer_recipe "MistralTokenizerRecipe" \
+--tokenizer_load '{"cache_dir": "../models"}' \
+--model_finetune_path "../models/mistral/mathqa/finetune_3shot/checkpoint-3357" \
+--model_peft_path "../models/mistral/mathqa/spt_3shot/checkpoint-3357" \
+--num_samples 250
+
+python similarity_cli.py -c configs/similarity_yaml.py \
+--output_path "../models/mistral_base/mathqa/result.json" \
+--dataset_name "math_qa" \
+--dataset_recipe "YAMLDatasetRecipe" \
+--dataset_load '{"yaml_path": "../lm-evaluation-harness-prompt-template/lm_eval/tasks/mathqa/mathqa.yaml"}' \
+--num_examples 3 \
+--model_name "mistralai/Mistral-7B-v0.1" \
+--tokenizer_name "mistralai/Mistral-7B-v0.1" \
+--model_recipe "MistralModelRecipe" \
+--model_template_recipe "MistralModelTemplateRecipe" \
+--model_load '{"cache_dir": "../models"}' \
+--tokenizer_recipe "MistralTokenizerRecipe" \
+--tokenizer_load '{"cache_dir": "../models"}' \
+--model_finetune_path "../models/mistral_base/mathqa/finetune_3shot/checkpoint-3357" \
+--model_peft_path "../models/mistral_base/mathqa/spt_3shot/checkpoint-3357" \
+--num_samples 250
+
+python similarity_cli.py -c configs/similarity_yaml.py \
+--output_path "../models/mistral_opt/mathqa/result.json" \
+--dataset_name "math_qa" \
+--dataset_recipe "YAMLDatasetRecipe" \
+--dataset_load '{"yaml_path": "../lm-evaluation-harness-prompt-template/lm_eval/tasks/mathqa/mathqa.yaml"}' \
+--num_examples 3 \
+--model_name "OpenPipe/mistral-ft-optimized-1227" \
+--tokenizer_name "OpenPipe/mistral-ft-optimized-1227" \
+--model_recipe "MistralModelRecipe" \
+--model_template_recipe "MistralModelTemplateRecipe" \
+--model_load '{"cache_dir": "../models"}' \
+--tokenizer_recipe "MistralTokenizerRecipe" \
+--tokenizer_load '{"cache_dir": "../models"}' \
+--model_finetune_path "../models/mistral_opt/mathqa/finetune_3shot/checkpoint-3357" \
+--model_peft_path "../models/mistral_opt/mathqa/spt_3shot/checkpoint-3357" \
+--num_samples 250
